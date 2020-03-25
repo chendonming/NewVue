9a1dcc491252e3208ca04b04874fbac144be038a
chendm <2283973011@qq.com>
2020-03-25 08:54:54 +0800

    test electron

--- engine/src/views/Home.vue
+++ engine/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -13,6 +13,15 @@ export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  created() {
+    const myNotification = new Notification('标题', {
+      body: '通知正文内容',
+    });
+
+    myNotification.onclick = () => {
+      console.log('通知被点击');
+    };
   },
 };
 </script>
